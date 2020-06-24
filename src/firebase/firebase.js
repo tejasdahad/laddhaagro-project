a374438aafe23fa01a9ac61fcef9a0b6e1c988dc
Tejas Dahad <tejasdahad000@gmail.com>
2020-06-24 10:41:01 +0530

    Added environment variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCxoQFqb2pjBEeu3vzdyUT14UnqB0pHQD8",
-    authDomain: "factory-7885e.firebaseapp.com",
-    databaseURL: "https://factory-7885e.firebaseio.com",
-    projectId: "factory-7885e",
-    storageBucket: "factory-7885e.appspot.com",
-    messagingSenderId: "143562971184",
-    appId: "1:143562971184:web:709538e8ed6e27e83435f5"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
